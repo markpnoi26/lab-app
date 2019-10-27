0820afde87df6b2feff5fcfb46ee4e9f4da36bea
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-26 20:26:12 -0700

    edits to scientist

--- app/models/scientist.rb
+++ app/models/scientist.rb
@@ -1,4 +1,13 @@
 class Scientist < ActiveRecord::Base
   has_secure_password
   has_many :projects
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find {|user| user.slug == slug}
+  end
+
 end
