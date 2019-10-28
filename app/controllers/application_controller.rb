019f55651d8b0c15051a104a9862dc6f41549419
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-28 00:30:45 -0700

    added some session error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,36 +15,50 @@ class ApplicationController < Sinatra::Base
 
   get "/register" do
     if session[:scientist_id]
+      session.delete(:register_condition) if session[:register_condition]
       redirect "/projects"
     else
+      @session_message = session[:register_condition]
       erb :"application/register"
     end
   end
 
   post "/register" do
+    @existing_scientist = Scientist.find_by(username: params[:username])
     if params[:name] == "" || params[:username] == "" || params[:email] == "" || params[:password] == ""
+      session[:register_condition] = "Cannot register, some fields were left empty."
+      redirect "/register"
+    elsif @existing_scientist
+      session[:register_condition] = "Username already exist, enter a different one."
       redirect "/register"
     else
       @scientist = Scientist.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
       session[:scientist_id] = @scientist.id
+      session.delete(:register_condition) if session[:register_condition]
       redirect "/projects"
     end
   end
 
   get "/signin" do
     if session[:scientist_id]
+      session.delete(:register_condition) if session[:register_condition]
+      session.delete(:sign_in_condition) if session[:sign_in_condition]
       redirect "/projects"
     else
+      @session_message = session[:sign_in_condition]
       erb :"application/signin"
     end
   end
 
   post "/signin" do
-    @scientist = Scientist.find_by(:username => params[:username])
+    @scientist = Scientist.find_by(username: params[:username])
     if @scientist && @scientist.authenticate(params[:password])
       session[:scientist_id] = @scientist.id
+      session.delete(:sign_in_condition) if session[:sign_in_condition]
+      session.delete(:register_condition) if session[:register_condition]
       redirect "/projects"
     else
+      session[:sign_in_condition] = "Username or Password was entered incorrectly."
       redirect "/signin"
     end
   end
