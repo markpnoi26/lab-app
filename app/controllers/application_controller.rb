fed85d1310842ee1c477d23adae7aff6e40735cb
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-10-27 21:44:56 -0700

    added major features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,53 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "super_secret_session_code"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  get "/register" do
+    if session[:scientist_id]
+      redirect "/projects"
+    else
+      erb :"application/register"
+    end
+  end
+
+  post "/register" do
+    if params[:name] == "" || params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect "/register"
+    else
+      @scientist = Scientist.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @scientist.id
+      redirect "/projects"
+    end
+  end
+
+  get "/signin" do
+    if session[:scientist_id]
+      redirect "/projects"
+    else
+      erb :"application/signin"
+    end
+  end
+
+  post "/signin" do
+    @scientist = Scientist.find_by(:username => params[:username])
+    if @scientist && @scientist.authenticate(params[:password])
+      session[:scientist_id] = @scientist.id
+      redirect "/projects"
+    else
+      redirect "/signin"
+    end
+  end
+
+  get "/signout" do
+    session.clear
+    redirect "/signin"
   end
 
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,36 @@
 class ProjectsController < ApplicationController
+
+  get "/projects" do
+    if session[:scientist_id]
+      @scientist = Scientist.find_by_id(session[:scientist_id])
+      @projects = Project.all
+      erb :"projects/index"
+    else
+      redirect "/signin"
+    end
+  end
+
+  get "/projects/new" do
+    if session[:scientist_id]
+      @scientist = Scientist.find_by_id(session[:scientist_id])
+      erb :"projects/new"
+    else
+      redirect "/signin"
+    end
+  end
+
+  get "/projects/:id" do
+    erb :"projects/show"
+  end
+
+  get "/projects/:id/edit" do
+    erb :"projects/edit"
+  end
+
+  patch "/projects/:id" do
+  end
+
+  delete "/projects/:id" do
+  end
+
 end
